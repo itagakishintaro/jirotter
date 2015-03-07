76c513d5e07c3cad228e91007f8a6e7e6b412b46
itagaki <itagaki.shintaro@gmail.com>
2015-03-07 12:58:10 +0900

    Add shops

--- js/jirotter.js
+++ js/jirotter.js
@@ -1,19 +1,50 @@
 'use strict';
 
-var FONT_SIZE = 16;
-var circle_data = [];
-var text_data = [];
+var shops = [
+'三田本店',
+'目黒店',
+'仙川店',
+'歌舞伎町店',
+'品川店',
+'新宿小滝橋通り店',
+'環七新代田店',
+'八王子野猿街道店２',
+'池袋東口店',
+'新小金井街道店',
+'亀戸店',
+'京急川崎店',
+'府中店',
+'松戸駅前店',
+'めじろ台法政大学前店',
+'荻窪店',
+'上野毛店',
+'京成大久保店',
+'環七一之江店',
+'相模大野店',
+'横浜関内店',
+'神田神保町店',
+'小岩店',
+'ひばりヶ丘駅前店',
+'桜台駅前店',
+'栃木街道店',
+'立川店',
+'大宮店',
+'千住大橋駅前店',
+'茨城守谷店',
+'湘南藤沢店',
+'西台駅前店',
+'中山駅前店',
+'新橋店',
+'仙台店',
+'赤羽店．',
+'札幌店',
+'会津若松駅前店',
+'JR西口蒲田店' 
+];
 
-// svgタグを生成
-var svg = d3.select('#d3')
-    .append('svg')
-    .attr('width', '100%')
-    .attr('height', '100%');
+$.getJSON('http://localhost:8000/twitter_search.php?kensaku=二郎', function(json) {
+console.log(json.statuses);
 
-$.getJSON('http://localhost:8080/twitter_search.php', function(json) {
-    json.statuses.forEach(function(element) {
-        iminos(element.text, element.favorite_count);
-    });
 });
 
 function iminos(text, favorite_count) {
@@ -26,95 +57,7 @@ function iminos(text, favorite_count) {
         },
         success: function(json) {
 console.log(json);
-        	var x = ($('#d3').width() - 100) * Math.random() + 50;
-        	var y = ($('#d3').height() - 100) * Math.random() + 50;
-        	var hinshi = json.results[0].morphemes.filter(function(element){
-				return element.hinshi !== 'その他';
-			});
-			var size = hinshi.length;
-
-        	// draw circle
-        	circle_data.push( { text: text, spn: json.results[0].spn, x: x, y: y, r: (1 + favorite_count) * 10 } );
-        	drawCircle(circle_data);
-
-        	// draw text
-			hinshi.forEach(function(element, index){
-				// text_data.push( { text: element.shuushi, x: x, y: y - size * FONT_SIZE / 2 + (index + 1) * FONT_SIZE } );
-				var pm = index % 2 === 0 ? -1 : 1;
-				text_data.push( { text: element.shuushi, x: x + pm * Math.random() * 50, y: y + pm * Math.random() * 50 } );
-			});
-        	// drawText(text_data);
+        	
         }
     });
-}
-
-function drawCircle(data) {
-    svg.selectAll('circle')
-        .data(data)
-        .enter().append('circle')
-        .attr('cx', function(d) {
-            return d.x;
-        })
-        .attr('cy', function(d) {
-            return d.y
-        })
-        .attr('r', function(d) {
-            return d.r;
-        })
-        .attr('fill', function(d) {
-            if (d.spn === '0') {
-                return '#ecf0f1';
-            } else if (d.spn === '1') {
-                return '#ecf0f1';
-            } else if (d.spn === '2') {
-                return '#73B6D9'
-            } else if (d.spn === '3') {
-                return '#FFE620';
-            } else if (d.spn === '4') {
-                return '#00AD8E';
-            } else {
-                return 'white';
-            }
-        })
-        .on('click', function(d) {
-            alert(d.text);
-        });
-}
-
-function drawText(data){
-	var color = 'rgb(' + String(Math.round(Math.random() * 255)) + ',' + String(Math.round(Math.random() * 255)) + ',' + String(Math.round(Math.random() * 255)) + ')';
-    svg.selectAll('text')
-        .data(data.concat(data))
-        .enter().append('text')
-        .style('font-size', FONT_SIZE + 'px')
-        .attr('fill', color)
-        .attr('class', 'wrap')
-        .attr('x', function(d) {
-        	return d.x
-        })
-        .attr('y', function(d) {
-        	return d.y;
-        })
-        .text(function(d) {
-        	return d.text;
-        })
-        .attr({
-            'dominant-baseline': 'middle'
-        });
-}
-
-function setColor(d){
-	if (d.spn === '0') {
-        return '#ecf0f1';
-    } else if (d.spn === '1') {
-        return '#ecf0f1';
-    } else if (d.spn === '2') {
-        return '#103D7C'
-    } else if (d.spn === '3') {
-        return '#FFE620';
-    } else if (d.spn === '4') {
-        return '#00AD8E';
-    } else {
-        return 'white';
-    }
 }
